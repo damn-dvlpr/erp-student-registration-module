883a4141908bda53169328060897040151609c87
damn-dvlpr <naimishrastogi12@gmail.com>
2020-03-29 02:03:46 +0530

    Normalising database

--- app.js
+++ app.js
@@ -6,7 +6,41 @@ mongoose.SchemaTypes.Email.defaults.message = 'Email address is invalid';
 const app=express();
 mongoose.connect("mongodb://localhost:27017/students");
 app.use(bodyParser.urlencoded({extended:true}));
-var studentSchema=new mongoose.Schema({                                      
+var GuardianSchema=new mongoose.Schema({
+    father: String,
+    mother:String,
+    relative:String,
+    address:{
+        father:{type:String},
+        relative:{type:String}
+    },
+    phone:{
+        father:
+            {
+                type:Number,
+                maxlength:10,
+                minlength:10
+            },
+        mother:
+            {
+                type:Number,
+                maxlength:10,
+                minlength:10
+            },
+        relative:
+            {
+                type:Number,
+                maxlength:10,
+                minlength:10
+            }
+    },
+    email:{
+        father:{type: mongoose.SchemaTypes.Email},
+        mother:{type: mongoose.SchemaTypes.Email},
+        relative:{type: mongoose.SchemaTypes.Email}
+    },
+});
+var PersonalSchema=new mongoose.Schema({                                      
     name: String,
     date_of_birth:Date,
     gender:{
@@ -52,21 +86,30 @@ var studentSchema=new mongoose.Schema({
         type:String,
     },
 });
-var Student=mongoose.model("Student",studentSchema);
+var Personal=mongoose.model("PersonalSchema",PersonalSchema);
+var Guardian=mongoose.model("GuardianSchema",GuardianSchema);
 app.get("/register",function(req,res){
     res.render("register.ejs");
 });
 app.post("/register",function(req,res){
     var student_info=req.body;
-    Student.create(student_info,function(err,returnedData){
+    console.log(req.body);
+    Personal.create(student_info,function(err,returnedData){
         if(err){
             console.log(err);
         }else{
+            Guardian.create(student_info,function(err,returnedData){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(returnedData);
+                }    
+            })
             console.log(returnedData);
             res.redirect("/register");
         }
     })
 });
 app.listen(8080,function(){
-    console.log("server running at 8080")
+    console.log("Server running at 8080")
 })
